c6945ce835d82f9578d5dcfaa499ab01c99df528
Foysal Bhuiyan <foysalbhuiyandev@gmail.com>
2022-09-08 00:54:23 +0600

    done

---
+++ js/cart.js
@@ -0,0 +1,15 @@
+const getInputValueById = id => {
+    const inputField = document.getElementById(id);
+    const inputValue = inputField.value;
+    inputField.value = '';
+    return inputValue;
+}
+
+const addProduct = () => {
+    const product = getInputValueById('product-name-field');
+    const quantity = getInputValueById('product-quantity-field');
+    console.log(product, quantity);
+
+
+    localStorage.setItem(quantity, product);
+}
--- js/storage.js
+++ js/storage.js
@@ -4,3 +4,14 @@ document.getElementById('btn-add-name').addEventListener('click', function () {
 
     localStorage.setItem('name', nameValue);
 })
+
+document.getElementById('btn-add-age').addEventListener('click', function () {
+    const ageField = document.getElementById('age-field');
+    const ageValue = ageField.value;
+
+    localStorage.setItem('age', ageValue);
+})
+
+document.getElementById('btn-remove-name').addEventListener('click', function () {
+    localStorage.removeItem('name');
+})
